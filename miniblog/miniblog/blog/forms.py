82bd3e7ef8daa3c3d90368ab3d424b09a77b4c68
somesh-py <someshsharma615@gmail.com>
2023-03-01 10:32:45 +0530

    create structure for form and modle

---
+++ miniblog/miniblog/blog/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField
+from django.contrib.auth.models import User
+from django.utils.translation import gettext,gettext_lazy as _
+from .models import Post
